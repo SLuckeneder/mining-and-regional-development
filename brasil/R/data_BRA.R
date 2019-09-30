62a62419feba07e42ceb1b7af8e50b6532ee8738
SLuckeneder <sebastian.luckeneder@wu.ac.at>
2019-09-30 16:56:42 +0200

    TBC

--- brasil/R/data_BRA.R
+++ brasil/R/data_BRA.R
@@ -3,6 +3,7 @@ library(sf)
 library(tidyverse)
 library(viridis)
 library(fineprintutils)
+library(stringr)
 
 # spatial polygon data frames ---------------------------------------------
 
@@ -122,69 +123,43 @@ sf_panel <- sf_panel %>% dplyr::left_join(snl_data_sf_agg_panel, by = c("GID_2",
 # ports <- read.csv("input/ports.csv", sep = ";")
 
 
-# OECD --------------------------------------------------------------------
 
-# region concordance and subset vector
-dstruc <- OECD::get_data_structure("REGION_DEMOGR") 
-reg <- dstruc$REG_ID
-reg_ids <- c(paste0("ME0", c(1:9)), paste0("ME", c(10:32)), 
-             paste0("PE0", c(1:9)), paste0("PE", c(10:25)), 
-             paste0("CL0", c(1:9)), paste0("CL", c(10:15)))
 
-# demography
+# IBGE demographics -------------------------------------------------------
 
-if("oecd_demography.RData" %in% dir("input/oecd")){
-  load("input/oecd/oecd_demography.RData")
-} else{
-  
-  dstruc <- OECD::get_data_structure("REGION_DEMOGR") 
-  var_desc <- dstruc$VAR_DESC # dstruc variable descriptions
-  var <- dstruc$VAR # list of available variables
-  filter_list <- list()
-  filter_list[[1]] <- 2 # TL2
-  filter_list[[2]] <- reg_ids # region
-  filter_list[[3]] <- c("POP_DEN", # population density (pop. per km2)
-                        "T") # population, all ages
-  filter_list[[4]] <- "T" # sex (M+F)
-  oecd_demography <- OECD::get_dataset("REGION_DEMOGR", filter = filter_list) %>% # download
-    dplyr::select(-TL, -SEX, -POS) %>%
-    dplyr::left_join(var, by = c("VAR" = "id"))
-  save(oecd_demography, file = "input/oecd/oecd_demography.RData")
-}
+ibge_pop <- read.csv("brasil/input/ibge/pop_mod.csv", sep = ";", skip = 1, stringsAsFactors = FALSE) 
+ibge_pop <- ibge_pop %>%
+  dplyr::select(c(1:6)) %>%
+  `colnames<-`(c("level", "id", "municipio", "year", "variable", "value")) %>%
+  dplyr::filter(level == "MU") %>%
+  dplyr::select(-level) %>%
+  dplyr::mutate(state = stringr::str_sub(municipio,-4)) %>%
+  dplyr::mutate(state = substr(state, 2, 3)) 
+ibge_pop <- ibge_pop %>%
+  dplyr::mutate(municipio = substr(municipio,1,nchar(ibge_pop$municipio)-4)) %>%
+  dplyr::mutate(value = as.numeric(value))
 
-# economy
 
-if("oecd_economy.RData" %in% dir("input/oecd")){
-  load("input/oecd/oecd_economy.RData")
-} else{
-  
-  dstruc <- OECD::get_data_structure("REGION_ECONOM") 
-  var_desc <- dstruc$VAR_DESC
-  var <- dstruc$VAR
-  filter_list <- list()
-  filter_list[[1]] <- 2 # TL2
-  filter_list[[2]] <- reg_ids # region
-  filter_list[[3]] <- "SNA_2008" # Last SNA classification (SNA 2008 or latest available)
-  filter_list[[4]] <- c("GDP", # Regional GDP
-                        "GVA_TOTAL", # Regional Gross Value Added, total activities
-                        "GVA_IND_10_VA", # GVA in agriculture, forestry and fishing (ISIC rev4)
-                        "GVA_IND_10_VB_E", # GVA in industry, including energy (ISIC rev4)
-                        "GVA_IND_10_VC", # ..Of which: GVA in manufacturing (ISIC rev4)
-                        "GVA_IND_10_VK") # GVA in financial and insurance activities (ISIC rev4)
-  filter_list[[5]] <- "USD_PPP" # measured in Millions USD, current prices, current PPP
-  filter_list[[6]] <- "ALL" # All regions
-  oecd_economy <- OECD::get_dataset("REGION_ECONOM", filter = filter_list) %>% # download
-    dplyr::select(-TL, -POS) %>%
-    dplyr::left_join(var, by = c("VAR" = "id"))
-  save(oecd_economy, file = "input/oecd/oecd_economy.RData")
-}
+# IBGE economics ----------------------------------------------------------
 
-suppressWarnings(rm(dstruc, filter_list, reg, var, var_desc))
+ibge_gdp <- read.csv("brasil/input/ibge/gdp_mod.csv", sep = ";", skip = 1, stringsAsFactors = FALSE) 
+ibge_gdp <- ibge_gdp %>%
+  `colnames<-`(c("level", "id", "municipio", "year", "variable", "value")) %>%
+  dplyr::filter(level == "MU") %>%
+  dplyr::select(-level) %>%
+  dplyr::mutate(state = stringr::str_sub(municipio,-4)) %>%
+  dplyr::mutate(state = substr(state, 2, 3)) 
+ibge_gdp <- ibge_gdp %>%
+  dplyr::mutate(municipio = substr(municipio,1,nchar(ibge_gdp$municipio)-4)) %>%
+  dplyr::mutate(value = as.numeric(value))
+
+
 
 # merge data --------------------------------------------------------------
 
-oecd_data <- dplyr::bind_rows(oecd_demography, oecd_economy)
+ibge_data <- dplyr::bind_rows(oecd_demography, oecd_economy)
 suppressWarnings(rm(oecd_demography, oecd_economy))
+
 
 # merge to spatial data ---------------------------------------------------
 
